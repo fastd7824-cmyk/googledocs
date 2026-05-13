e0a615f71ff7de9f8ead9ec6c4718612e6cd09da
dejamach <fastd7824@gmail.com>
2026-05-13 19:49:00 +0300

    updated

--- src/java/com/app/config/DatabaseConfig.java
+++ src/java/com/app/config/DatabaseConfig.java
@@ -6,31 +6,42 @@ import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class DatabaseConfig {
+
     private static final Logger LOGGER = Logger.getLogger(DatabaseConfig.class.getName());
-    private static final String[] MYSQL_DRIVERS = {
-        "com.mysql.jdbc.Driver",
-        "com.mysql.jdbc.Driver"
-    };
-    private static final String URL = "jdbc:mysql://yamabiko.proxy.rlwy.net:46438/railway?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
+
+    private static final String URL =
+            "jdbc:mysql://yamabiko.proxy.rlwy.net:46438/railway"
+            + "?useSSL=false"
+            + "&allowPublicKeyRetrieval=true"
+            + "&serverTimezone=UTC"
+            + "&useUnicode=true"
+            + "&characterEncoding=UTF-8";
+
     private static final String USER = "root";
     private static final String PASSWORD = "sKxIkdmTgTPFLUVIYjhNPJCndqpmDKUQ";
 
     static {
         boolean driverLoaded = false;
-        for (String driverClass : MYSQL_DRIVERS) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            driverLoaded = true;
+            LOGGER.info("MySQL JDBC Driver loaded: com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException modernDriverMissing) {
+
             try {
-                Class.forName(driverClass);
-                LOGGER.info("MySQL JDBC Driver registered: " + driverClass);
+                Class.forName("com.mysql.jdbc.Driver");
                 driverLoaded = true;
-                break;
-            } catch (ClassNotFoundException ignored) {
-                // Try the next supported MySQL driver class name.
+                LOGGER.warning("Using legacy MySQL JDBC Driver: com.mysql.jdbc.Driver");
+            } catch (ClassNotFoundException legacyDriverMissing) {
+                LOGGER.severe("MySQL JDBC driver not found.");
             }
         }
 
+        DriverManager.setLoginTimeout(2);
+
         if (!driverLoaded) {
-            LOGGER.severe("MySQL JDBC Driver not found. Tried: com.mysql.jdbc.Driver, com.mysql.jdbc.Driver");
-            throw new RuntimeException("MySQL JDBC Driver not found");
+            throw new RuntimeException("Unable to load MySQL JDBC Driver");
         }
     }
 
@@ -39,7 +50,6 @@ public class DatabaseConfig {
     }
 
     public static void closePool() {
-        // Nothing to close - connections are closed individually.
         LOGGER.info("No connection pool to close.");
     }
 }
